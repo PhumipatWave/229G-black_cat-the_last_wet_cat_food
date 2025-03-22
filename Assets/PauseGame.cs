e1043d2185b74faa3503bbc9a855d14788c7c17f
1660701663Suwaphat <suwaphat.chok@bumail.net>
2025-03-23 01:15:53 +0700

    เพิ่มการทำงานของปุ่ม Start และ Pause

---
+++ Assets/PauseGame.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class PauseGame : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    private bool isPaused = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    void ResumeGame()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+}
